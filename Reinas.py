56482476a232812849d04ea8442f9eb0c5a58ad8
GustavoSepul <gsepulvedao@ing.ucsc.cl>
2022-09-25 18:58:39 -0300

    funcion fitness
    funcion para calcular el fitness

--- Reinas.py
+++ Reinas.py
@@ -5,8 +5,7 @@ import numpy as np
 
 seed = 1
 n = 8
-p = 8
-print(seed, n, p)
+p = 4
 
 np.random.seed(seed)
 
@@ -17,3 +16,24 @@ for k in range(p):
     np.random.shuffle(poblacion[k])
     
 print(poblacion)
+ValorFitness=[]
+fitness=[]
+for h in range(p):
+    ValorFitness.append(0) 
+    
+def FitnessFunction(poblacion):
+    for i in range(0, 4):
+        for j in range(0, 7):
+            for k in range(j+1, 8):
+                if poblacion[i][j]==poblacion[i][k]:
+                    ValorFitness[i] += 1
+                if poblacion[i][j]-poblacion[i][k] == j-k or poblacion[i][j]-poblacion[i][k] == k-j:
+                    ValorFitness[i] += 1
+                    
+    for q in range(p):
+        fitness.append(ValorFitness[q])
+    return fitness
+
+f = FitnessFunction(poblacion)
+
+print(f)
