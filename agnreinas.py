fb305663988276fd43122b0ef72c8e3f7be06d09
DiegoAraneda98 <daranedah@ing.ucsc.cl>
2022-09-28 18:35:45 -0300

    Update agnreinas.py

--- agnreinas.py
+++ agnreinas.py
@@ -1,3 +1,4 @@
+
 import sys
 import random
 import time
@@ -8,9 +9,9 @@ from fitness import *
 if len(sys.argv) == 4:
     semilla=int(sys.argv[1])
     tamaño_tablero=int(sys.argv[2])
-    tamaño_población=int(sys.argv[3])
+    tamaño_poblacion=int(sys.argv[3])
   
-    print(semilla,tamaño_tablero,tamaño_población)
+    print(semilla,tamaño_tablero,tamaño_poblacion)
 else:
     print("Error")
     print("Ingrese denuevo los parametros")
@@ -19,26 +20,38 @@ else:
 tiempo_proceso_ini = time.process_time()
 np.random.seed(semilla)
 
-poblacion = np.zeros((tamaño_población,tamaño_tablero),int)
+poblacion = np.zeros((tamaño_poblacion,tamaño_tablero),int)
 
-for k in range(tamaño_población):
+for k in range(tamaño_poblacion):
     poblacion[k]= np.arange(0,tamaño_tablero)
     np.random.shuffle(poblacion[k])
 
 print(poblacion)
 
-f = FuncionFitness(poblacion, tamaño_población, tamaño_tablero)
+f = FuncionFitness(poblacion, tamaño_poblacion, tamaño_tablero)
+
 
 print(f)
 
-def FuncionSeleccion(f,tamazo_poblacion):
-    probabilidades = []
+def FuncionSeleccion(f,tamaño_poblacion):
     probabilidad = np.sum(f)
+    probabilidades = f/probabilidad
+    probabilities = [sum(probabilidades[:i+1]) 
+                     for i in range(len(probabilidades))]
+    pr = probabilities
+    print(pr)
+    r = random.random()
+    print(r)
+    seleccionado = []
+    for (i, individual) in enumerate(poblacion):
+        if r <= pr[i]:
+            seleccionado.append(list(individual))
+            break
+    return seleccionado
     
-    probabilidades.append(f/probabilidad)
-    return probabilidades
+selec = FuncionSeleccion(f,tamaño_poblacion)
+print(selec)
 
-pr = FuncionSeleccion(f,p)
-print(pr[0])
+selec2= FuncionSeleccion(f,tamaño_poblacion)
 
-
+print(selec2)
