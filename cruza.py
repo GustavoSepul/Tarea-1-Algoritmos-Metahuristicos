7fd1f0492a36c08698a4dbf5689447966e74f1f5
Javier-vic <93997406+Javier-vic@users.noreply.github.com>
2022-09-29 16:57:28 -0400

    Arreglo funcion cruza
    Se arreglo la funcion cruza para su uso en ruleta

--- cruza.py
+++ cruza.py
@@ -1,14 +1,16 @@
 import numpy as np
 import random
 
-def cruza_padres (p1, p2, p_cruza):
-    c1, c2 = p1.copy (), p2.copy ()
-    if(random.random()<p_cruza) :
-        pt = np.random.randint (1, len (p1))
-        print(pt)
-        c1 = np.concatenate([p1[:pt], p2[pt:]])
-        c2 = np.concatenate([p2[:pt], p1[pt:]])
-    child1 = c1
-    child2 = c2
-    return [child1, child2]
+def cruza_padres (padres):
+    c1 = padres[0] 
+    c2 = padres[1]
+    hijos = []
+    
+    pt = np.random.randint (1, len (padres.T))
+    print(pt)
+    c1 = np.concatenate([padres[0][:pt], padres[1][pt:]])
+    c2 = np.concatenate([padres[1][:pt], padres[0][pt:]])
+    hijos.append(c1)
+    hijos.append(c2)
+    return hijos
 
