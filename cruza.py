90b86866b48c0dbaa7111570c48c8472d01ac0b5
DiegoAraneda98 <daranedah@ing.ucsc.cl>
2022-09-29 17:13:57 -0300

    nueva cruza

--- cruza.py
+++ cruza.py
@@ -1,12 +1,14 @@
 import numpy as np
+import random
 
-def cruza(padre1, padre2, p_cruza):
-    r = np.random.random()
-    if(r < p_cruza):
-        nc_random = np.random.randint(1, n)
-        hijo1 = padre1([padre1[:nc_random], padre2[nc_random:]])
-        hijo2 = padre2([padre2[nc_random:], padre1[:nc_random]])
-    else:
-        hijo1 = padre1.copy()
-        hijo2 = padre2.copy()
-    return hijo1, hijo2
+def crossover (p1, p2, p_cruza):
+    c1, c2 = p1.copy (), p2.copy ()
+    if(random.random()<p_cruza) :
+        pt = np.random.randint (1, len (p1))
+        print(pt)
+        c1 = np.concatenate([p1[:pt], p2[pt:]])
+        c2 = np.concatenate([p2[:pt], p1[pt:]])
+    child1 = c1
+    child2 = c2
+    return [child1, child2]
+
