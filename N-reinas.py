194a80d10bd4abae28b5d36abe32b11d043bea84
DiegoAraneda98 <daranedah@ing.ucsc.cl>
2022-09-29 18:38:13 -0300

    update

---
+++ N-reinas.py
@@ -0,0 +1,28 @@
+import numpy as np
+import sys
+import random
+import time
+""" np.zeros np.aranger np.random.seed np.random.shuffle"""
+
+if len(sys.argv) == 4:
+    semilla=int(sys.argv[1])
+    n=int(sys.argv[2])
+    p=int(sys.argv[3])
+    print(semilla,n,p)
+else:
+    print("Error")
+    print("Ingrese denuevo los parametros")
+    sys.exit(0)
+
+np.random.seed(semilla)
+
+  
+poblacion = np.zeros((p,n),int)
+
+for k in range(p):
+    poblacion[k] = np.aranger(0,n)
+    np.random.shuffle(poblacion[k])
+
+print(poblacion)
+
+
