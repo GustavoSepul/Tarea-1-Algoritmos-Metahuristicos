d628d22ca14f26b63ea0cb8f1efef795d7125d92
Javier-vic <93997406+Javier-vic@users.noreply.github.com>
2022-09-29 22:38:09 -0400

    Arreglo funcion mutacion

--- mutacion.py
+++ mutacion.py
@@ -1,17 +1,15 @@
 import numpy as np
-def mutacion(hijo_rec, p_mutacion):
-    nm_random = np.random.random()
-    print(nm_random)
-    if(nm_random < p_mutacion):
-        m = np.random.randint(0, hijo_rec.size)
-        n = np.random.randint(0, hijo_rec.size)
-        while m == n:
-            n = np.random.randint(0, hijo_rec.size)
-        hijo_rec[m],hijo_rec[n] = hijo_rec[n],hijo_rec[m]
-    return hijo_rec
 
-padres = np.arange(3 * 4).reshape(3, 4)
-print(padres[0].size)
-print(padres[0])
-aa = mutacion(padres[0],0.5)
-print(aa)
+def mutacion(poblacion, p_mutacion):
+    for i in range(len(poblacion)):
+        nm_random = np.random.random()
+        # print(nm_random)
+        if(nm_random < p_mutacion):
+            m = np.random.randint(0, len(poblacion.T)-1)
+            n = np.random.randint(0, len(poblacion.T)-1)
+            while m == n:
+                n = np.random.randint(0, len(poblacion.T)-1)
+            hijo_mut = poblacion[i]
+            hijo_mut[m],hijo_mut[n] = hijo_mut[n],hijo_mut[m]
+            poblacion[i] = hijo_mut
+    return poblacion
